20c3ba66e0502af0193d5d977756496ab5106eaf
Kurva2076 <kotplombir@gmail.com>
2023-12-11 20:21:03 +0300

    Интегрировал старые html и css коды, шрифт Montserrat, а также необходимые картинки

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/img/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 
   <FormBlock />
 </template>
