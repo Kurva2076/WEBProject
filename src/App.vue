712f6e23578b3bcd3f1f77955bfa5a1a0c28e271
Kurva2076 <kotplombir@gmail.com>
2023-12-12 06:59:14 +0300

    В блоке с сервисной инфорацией убрал псевдо элемменты ::marker и добавил ::before, добавив необходимые картинки Также добавил немного адаптивности (брекпоинтов с принципом first-mobile)

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,40 @@
 <template>
-  <!--  <img alt="Vue logo" src="./assets/img/logo.png">-->
-  <!--  <HelloWorld msg="Welcome to Your Vue.js App" />-->
+  <img alt="Vue logo" src="./assets/img/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 
   <FormBlock />
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue'
 import FormBlock from "@/components/FormBlock.vue";
 
 export default {
   name: 'App',
   components: {
-    FormBlock
+    FormBlock,
+    HelloWorld
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-
-}
-
 @font-face {
-  font-family: Montserrat;
+  font-family: "Montserrat";
   src: url("./assets/fonts/Montserrat/Montserrat-Regular.ttf") format("truetype");
 }
 
 body {
   margin: 0;
-  font-family: Montserrat, serif !important;
+  font-family: "Montserrat", system-ui !important;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
