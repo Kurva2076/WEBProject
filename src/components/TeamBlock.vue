30d181513931f5d14e6d3a8039fe41bf52970ee5
Kurva2076 <kotplombir@gmail.com>
2023-12-24 01:13:55 +0300

    Написал предварительный html код блока

--- src/components/TeamBlock.vue
+++ src/components/TeamBlock.vue
@@ -1,5 +1,55 @@
 <template>
-Команда
+  <section>
+    <div class="team-block-container container">
+      <div class="title">Команда</div>
+
+      <div class="team-block row">
+        <div class="employee">
+          <img class="employee-image" src="../assets/img/Sergey-Sinitsa.webp" alt="Path to image of Sergey Sinitsa" />
+
+          <div class="employee-position">
+            Руководитель отдела веб-разработки, канд. техн. наук, заместитель директора
+          </div>
+        </div>
+
+        <div class="employee">
+          <img class="employee-image" src="../assets/img/Roman-Agabekov.webp" alt="Path to image of Sergey Sinitsa" />
+
+          <div class="employee-position">
+            Руководитель отдела DevOPS, директор
+          </div>
+        </div>
+
+        <div class="employee">
+          <img class="employee-image" src="../assets/img/Alexey-Sinitsa.webp" alt="Path to image of Sergey Sinitsa" />
+
+          <div class="employee-position">
+            Руководитель отдела поддержки сайтов
+          </div>
+        </div>
+
+        <div class="employee">
+          <img class="employee-image" src="../assets/img/Daria-Bochkareva.webp" alt="Path to image of Sergey Sinitsa" />
+
+          <div class="employee-position">
+            Руководитель отдела продвижения, контекстной рекламы и контент-поддержки сайтов
+          </div>
+        </div>
+
+        <div class="employee">
+          <img class="employee-image" src="../assets/img/Irina-Torkunova.webp" alt="Path to image of Sergey Sinitsa" />
+
+          <div class="employee-position">
+            Менеджер по работе с клиентами
+          </div>
+        </div>
+      </div>
+
+      <div class="employees-button">
+        <button type="button">Вся команда</button>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
