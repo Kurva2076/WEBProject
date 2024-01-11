a5accf1b16dafa294604c9e55885c14919502d52
Vlad <gurinvladis185@yandex.ru>
2024-01-11 06:07:15 +0300

    Сделал адаптивность под разные размеры экрана(но не получилось полностью хорошо)

--- src/components/FAQ.vue
+++ src/components/FAQ.vue
@@ -21,6 +21,11 @@ function accordion(event) {
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"></script>
   </head>
   <div class = "container1">FAQ</div>
   <div class="container2"></div>
@@ -161,5 +166,61 @@ summary:focus {
 details:focus-within{
   border:solid orangered;
   color: orangered;
+}
+@media(min-width:480px){
+  .container2{
+    height: 100%;
+  }
+  details{
+    font-size: 25px;
+    height: auto;
+  }
+  summary{
+    font-size: 31px;
+    height: auto;
+  }
+}
+@media(max-width:479px){
+  .container2{
+    height: 100%;
+  }
+  details{
+    font-size: 30px;
+    height: auto;
+  }
+  summary{
+    font-size: 36px;
+    height: auto;
+  }
+}
+
+@media(min-width:768px){
+  .container2{
+    margin: 9px;
+    padding: 15px;
+    height: 100%;
+  }
+  details{
+    font-size: 20px;
+    height: auto;
+  }
+  summary{
+    font-size: 26px;
+    height: auto;
+  }
+}
+@media(min-width:992px){
+  .container2{
+    margin: 11px;
+    padding: 15px;
+  }
+  details{
+    font-size: 15px;
+    height: auto;
+  }
+  summary{
+    font-size: 21px;
+    height: auto;
+  }
 }
 </style>
