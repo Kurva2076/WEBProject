382cd35b5e0b4db839504cce2b041d403426f2c9
Vlad <gurinvladis185@yandex.ru>
2024-01-12 05:53:20 +0300

    Сделал основу html

---
+++ razrabotka/src/components/razrabotka.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="main-function">
+    <div class="container content_function" style="margin:0px;">
+      <div class="row " style="margin-bottom:50px;">
+        <h2 class="title-function">Наши профессиональные разработчики выполняют быстро любые задачи</h2>
+        <div class="row list_function">
+          <div class ="col-xs-6 col-md-3 blocks" >
+            <div class="top_content">
+              <div class="icon">
+                <img  src="competency-20.svg"/>
+              </div>
+            </div>
+            <div class="bottom_title">от 1ч</div>
+            <div class="bottom_content"> Настройка события GA<br>
+              в интернет-магазине
+            </div>
+          </div>
+          <div class ="col-xs-6 col-md-3  blocks"  >
+            <div class="top_content">
+              <div class="icon">
+                <img  src="competency-21.svg"/>
+              </div>
+            </div>
+            <div class="bottom_title">от 20ч</div>
+            <div class="bottom_content">
+              Разработка мобильной<br>
+              версии сайта
+            </div>
+          </div>
+          <div class ="col-xs-6 col-md-3 blocks"  >
+            <div class="top_content">
+              <div class="icon">
+                <img  src="competency-22.svg"/>
+              </div>
+            </div>
+            <div class="bottom_title">от 8ч</div>
+            <div class="bottom_content">
+              Интеграция<br>
+              Модуля оплаты
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
